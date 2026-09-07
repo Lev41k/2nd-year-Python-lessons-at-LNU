00fbd5475d278cb5ed400f5a9b6fcdbf612fd98a
Lev <lev.naumko@lnu.edu.ua>
2026-09-07 11:23:23 +0300

    Added first 5 tasks os Lesson 1

---
+++ Lesson_1.py
@@ -0,0 +1,103 @@
+# 1 a
+# import math
+
+# print("Enter x:")
+# x = int(input())
+
+# print("Enter y:")
+# y = int(input())
+
+# r = ((math.sqrt(abs(x-1))) - ((abs(y)))**(1/3)) / (1 + x*x/2 + y*y/4)
+# print("Result: ", r)
+
+
+# 1 b
+# import math
+
+# x = int(input("Enter x:"))
+
+# z = float(input("Enter z:"))
+
+# r = x * (math.atan(z) + math.exp(-(x+3)))
+# print("Result: ", r)
+
+
+# 2 a
+# import math
+
+# x = float(input("Enter x:"))
+# y = float(input("Enter y:"))
+# z = float(input("Enter z:"))
+
+# r = (3 + math.exp(y-1)) / (1 + x*x*abs(y-math.tan(z)))
+# print("Result: ", r)
+
+
+# 2 b
+# import math
+
+# x = float(input("Enter x:"))
+# y = float(input("Enter y:"))
+
+# r = 1 + abs(y-x) + ((y-x)**2)/2 + ((abs(y-x))**3)/3
+# print("Result: ", r)
+
+
+# 3 a
+# import math
+
+# x = float(input("Enter x:"))
+# y = float(input("Enter y:"))
+
+# r = (1 + y)(((x + (y)/(x*x + 4)))/((math.exp(-x - 2) + 1)/(x*x + 4)))
+# print("Result: ", r)
+
+
+# 3 b
+# import math
+
+# x = float(input("Enter x:"))
+# y = float(input("Enter y:"))
+# z = float(input("Enter z:"))
+
+# r = (1 + math.cos(y-2))/((x**4)/2 + (math.sin(z))**2)
+# print("Result: ", r)
+
+
+# 4 a
+# import math
+
+# x = float(input("Enter x:"))
+# y = float(input("Enter y:"))
+
+# r = y + x/((y**2) + abs((x**2)/(y + (x**3)/3)))
+# print("Result: ", r)
+
+
+# 4 b
+# import math
+
+# x = float(input("Enter x:"))
+# y = float(input("Enter y:"))
+# z = float(input("Enter z:"))
+
+# r = 1 + (math.tan(z/2))**2
+# print("Result: ", r)
+
+
+# 5 a
+# import math
+
+# a = float(input("Enter a:"))
+
+# r = 2 * ((math.sin(3*(math.pi) - 2*a))**2) * ((math.cos(5*(math.pi) + 2*a))**2)
+# print("Result: ", r)
+
+
+# 5 b
+# import math
+
+# a = float(input("Enter a:"))
+
+# r = 1/2 - (1/4)*(math.sin(5/2*(math/pi) - 8*a))
+# print("Result: ", r)
